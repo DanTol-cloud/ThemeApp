6b5acd16a1b35a7ffbd6eadd74e6e475dae12174
DanTol-cloud <danielkozac05@gmail.com>
2024-01-17 12:17:12 +0200

    themeApp-1-0-0

---
+++ src/components/ChatBlock/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+
+const ChatBlock = () => {
+
+  return (
+    <View>
+      <Text>Hello</Text>
+      <View>
+        <Text>Hey, wat`s up</Text>
+      </View>
+    </View>
+  )
+}
+
+export default ChatBlock;
